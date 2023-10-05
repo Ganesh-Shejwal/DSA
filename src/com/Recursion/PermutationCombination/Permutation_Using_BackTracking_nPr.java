d23ffd0552dbfaebd1a093f7e0465cedea59009a
Ganesh Shejwal <ganeshshejwal201@gmail.com>
2023-10-05 16:37:34 +0530

    Recursion

---
+++ src/com/Recursion/PermutationCombination/Permutation_Using_BackTracking_nPr.java
@@ -0,0 +1,27 @@
+package com.Recursion.PermutationCombination;
+
+import java.util.Arrays;
+
+public class Permutation_Using_BackTracking_nPr {
+    // T.C. O(n!/(n-r)!
+    // S.C. O(n) => n = total items;
+    public static void main(String[] args) {
+        int current_item=1,total_items=2;
+        int[] boxes = new int[4];
+        help(current_item,total_items,boxes);
+    }
+    public static void help(int citem,int titems,int[] boxes){
+        if(citem>titems){
+            System.out.println(Arrays.toString(boxes));
+            return;
+        }
+        for(int i=0;i<boxes.length;i++){
+            if(boxes[i]==0){
+                boxes[i]=citem;
+                help(citem+1,titems,boxes);
+                boxes[i]=0;
+            }
+        }
+
+    }
+}
